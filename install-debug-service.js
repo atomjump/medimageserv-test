8251d0d4e0cae1020b7ac4738b3b02b72811e07d
AtomJump <peter@atomjump.com>
2017-12-05 18:54:10 +1300

    test service for errors

--- install-debug-service.js
+++ install-debug-service.js
@@ -27,6 +27,7 @@ var svc = new Service({
 // Listen for the "install" event, which indicates the
 // process is available as a service.
 svc.on('install',function(){
+  console.log("Installed. Starting...");
   svc.start();
 });
 
